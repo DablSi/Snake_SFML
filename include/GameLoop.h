a54b82aa923a359c2a5e3f344c475370ef1924f8
ntngel1 <ntngel1@gmail.com>
2018-08-31 12:28:29 +0300

    Added GameLoop

---
+++ include/GameLoop.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+class GameLoop {
+public:
+    GameLoop();
+    ~GameLoop();
+    
+    void load_resources();
+    void init();
+    void update();
+    void render(sf::RenderTarget* renderer);
+};
---
+++ src/GameLoop.cpp
@@ -0,0 +1,30 @@
+#include "GameLoop.h"
+
+GameLoop::GameLoop()
+{
+
+}
+
+GameLoop::~GameLoop()
+{
+
+}
+
+void GameLoop::load_resources()
+{
+
+}
+
+void GameLoop::init()
+{
+
+}
+
+void GameLoop::update()
+{
+}
+
+void GameLoop::render(sf::RenderTarget* renderer)
+{
+    renderer->clear(sf::Color(255,0,0));
+}
