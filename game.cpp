c949250693a54d1af782e50777627aa0c2bcc294
ntngel1 <ntngel1@gmail.com>
2018-08-31 09:16:53 +0300

    Simple window with SFML

--- game.cpp
+++ game.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    sf::RenderWindow* win = new sf::RenderWindow(sf::VideoMode(800, 600), "Test Win");
+
+    while (win->isOpen())
+    {
+        sf::Event e;
+        win->pollEvent(e);
+
+        if (e.type == sf::Event::EventType::Closed)
+        {
+            win->close();
+        }
+
+        win->clear(sf::Color(255,0,0));
+        win->display();
+    }
+
     return 0;
 }
